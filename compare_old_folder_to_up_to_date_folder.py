80de8536b9f1a9ab8149e06bd3f7b3b1bee04170
adrian88888888 <63675374+adrian88888888@users.noreply.github.com>
2022-05-04 21:52:27 -0300

    .

--- compare_old_folder_to_up_to_date_folder.py
+++ compare_old_folder_to_up_to_date_folder.py
@@ -6,41 +6,44 @@ import logging
 logging.basicConfig(level=logging.WARNING, format='%(message)s')
 # logging.basicConfig(level=logging.CRITICAL, format='%(message)s')
 
-slave_folder = r'C:\Users\adria\Desktop\Slave Folder....'
-main_folder  = r'C:\Users\adria\Desktop\Main Folder.....'
+# Settings
+OLD_FOLDER          = r'C:\Users\adria\Desktop\Slave Folder....'
+UP_TO_DATE_FOLDER   = r'C:\Users\adria\Desktop\Main Folder.....'
+COMPARE_SUBFOLDERS  = True
 
-def compare_slave_folder_to_main_folder():
-    logging.warning('Comparing slave folder to main folder...')
-    all_filepaths = get_filepaths_from(slave_folder)
+def compare_old_folder_to_up_to_date_folder():
+    logging.warning('Comparing old folder to up to date folder...')
+    all_filepaths = get_filepaths_from(OLD_FOLDER)
     for each_filepath in all_filepaths:
         filename = get_filename_from(each_filepath)
-        file_found = search_filename_in_main_folder(filename, main_folder)
+        file_found = search_filename_in_up_to_date_folder(filename, UP_TO_DATE_FOLDER)
         if file_found:
-            logging.info(filename + ' found in the main folder!')
+            logging.info(filename + ' found in the up to date folder!')
             logging.info('-----------------------')
         if not file_found:
-            logging.warning('The file ' + filename + ' is not in the main folder')
+            logging.warning('The file ' + filename + ' is not in the up to date folder')
             logging.warning('-----------------------')
             # move_file(each_filepath, filename) # ===> si queres podes mover los archivos que se encuentren, o los no se encuentren, a algun lado
 
 def get_filepaths_from(folder):
     logging.warning('-----------------------')
-    logging.warning('Getting list of filepaths from(in slave folder): ' + folder)
+    logging.warning('Getting list of filepaths from(in old folder): ' + folder)
     filepaths = []
     for current_directory, folders, files in os.walk(folder):
         for each_file in files:
             filepath = os.path.join(current_directory, each_file)
             filepaths.append(filepath)
-        break # ===> sacar esto para mirar dentro de las subcarpetas o dejarlo para mirar solo en el primer nivel
+        if not COMPARE_SUBFOLDERS:
+            break
     return filepaths
 
 def get_filename_from(filepath):
     logging.info('Getting filename from filepath...')
     return os.path.basename(filepath)
 
-def search_filename_in_main_folder(filename, main_folder):
-    logging.info('Searching ' + filename + ' in the main folder...')
-    for current_directory, folders, files in os.walk(main_folder):
+def search_filename_in_up_to_date_folder(filename, UP_TO_DATE_FOLDER):
+    logging.info('Searching ' + filename + ' in the up to date folder...')
+    for current_directory, folders, files in os.walk(UP_TO_DATE_FOLDER):
         if filename in files:
             return os.path.join(current_directory, filename)
 
@@ -55,13 +58,13 @@ def move_file(each_filepath, filename):
 # funciones que no se usan en el script pero que podrian ser
 # utiles si quiero tener en cuenta los archivos ocultos:
 
-def subtract_hidden_and_system_files(main_folders):
+def subtract_hidden_and_system_files(up_to_date_folders):
     logging.debug('Subtracting hidden and system folders')
-    user_main_folders = []
-    for each_folderpath in main_folders:
+    user_up_to_date_folders = []
+    for each_folderpath in up_to_date_folders:
         if not is_hidden(each_folderpath):
-            user_main_folders.append(each_folderpath)
-    return user_main_folders
+            user_up_to_date_folders.append(each_folderpath)
+    return user_up_to_date_folders
 
 def is_hidden(path):
     name = os.path.basename(os.path.abspath(path))
@@ -71,4 +74,4 @@ def has_hidden_attribute(path):
     return bool(os.stat(path).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
 
 
-compare_slave_folder_to_main_folder()
+compare_old_folder_to_up_to_date_folder()
